eedeaafb4fcbd3674600ad14f4dba9da85353af3
Ryan Cahela <6194542+RyanCahela@users.noreply.github.com>
2023-02-16 19:02:24 -0800

    added get-request code

---
+++ chapter_6/get_and_post_processing/get-request.php
@@ -0,0 +1,20 @@
+<?php 
+  $city = isset($_GET['city']) ? $_GET['city'] : '';
+
+  if(!$city) {
+    echo "<p style=\"color: red; font-weight: bold;\">Error!</p>";
+    exit;
+  }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Get Request</title>
+</head>
+<body>
+  <?= "<p>Hello from $city!</p>" ?> 
+</body>
+</html>
---
+++ chapter_6/get_and_post_processing/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Index</title>
+</head>
+<body>
+  <h1>Chapter 6 - Get and Post Processing</h1>
+  <h2>Get Request</h2>
+    <ul>
+      <li><a href="get-request.php/?city=London">London</a></li>
+      <li><a href="get-request.php/?city=Los+Angeles">Los Angeles</a></li>
+      <li><a href="get-request.php/?city=New+York+City">New York City</a></li>
+    </ul>
+  <h2>Post Request</h2>
+  <form action="post-request.php">
+
+  </form>
+
+  
+</body>
+</html>
